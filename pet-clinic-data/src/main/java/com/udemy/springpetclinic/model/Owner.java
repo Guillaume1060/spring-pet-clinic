fbdbd67df4c03e37acb51422809074a28464815a
Guillaume <breyerguillaume@gmail.com>
2023-10-05 14:25:06 +0200

    Adding properties. Closes #28.

--- pet-clinic-data/src/main/java/com/udemy/springpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/udemy/springpetclinic/model/Owner.java
@@ -6,7 +6,23 @@ import java.util.Set;
 
 @Getter
 public class Owner extends Person{
+
+    private String address;
+    private String city;
+    private String telephone;
     private Set<Pet> pets;
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public void setPets(Set<Pet> pets) {
         this.pets = pets;
