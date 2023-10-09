3c0c6a90adbbf57a0ba23a8300ca4ab1686bdc85
Guillaume <breyerguillaume@gmail.com>
2023-10-09 09:05:15 +0200

    Updated base entity with JPA annotations.Closes #35.

--- pet-clinic-data/src/main/java/com/udemy/springpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/udemy/springpetclinic/model/BaseEntity.java
@@ -1,11 +1,18 @@
 package com.udemy.springpetclinic.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 
 import java.io.Serializable;
 
 @Getter
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public void setId(Long id) {
