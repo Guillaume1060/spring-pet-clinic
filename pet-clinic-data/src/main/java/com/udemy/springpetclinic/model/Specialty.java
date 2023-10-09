33b0ecfaedc34364569a12eeb6bb77893a48bc07
Guillaume <breyerguillaume@gmail.com>
2023-10-09 10:44:40 +0200

    Updated vet with JPA entity mappings.Closes #37.

--- pet-clinic-data/src/main/java/com/udemy/springpetclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/udemy/springpetclinic/model/Specialty.java
@@ -1,9 +1,15 @@
 package com.udemy.springpetclinic.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.Getter;
 
 @Getter
+@Entity
+@Table(name = "specialties")
 public class Specialty extends BaseEntity{
+    @Column(name = "description")
     private String description;
 
     public void setDescription(String description) {
--- pet-clinic-data/src/main/java/com/udemy/springpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/udemy/springpetclinic/model/Vet.java
@@ -1,12 +1,18 @@
 package com.udemy.springpetclinic.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Getter
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Specialty> specialties = new HashSet<>();
 
     public void setSpecialties(Set<Specialty> specialties) {
