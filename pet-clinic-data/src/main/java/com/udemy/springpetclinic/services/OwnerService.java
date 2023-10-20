957713690924313b2b047173395b79bfa4ed1fff
Guillaume <breyerguillaume@gmail.com>
2023-10-20 14:57:29 +0200

    Impl find owner. Closes #54.

--- pet-clinic-data/src/main/java/com/udemy/springpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/udemy/springpetclinic/services/OwnerService.java
@@ -2,6 +2,9 @@ package com.udemy.springpetclinic.services;
 
 import com.udemy.springpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner,Long> {
     Owner findByLastName(String lastName);
+    List<Owner> findAllByLastNameLike(String LastName);
 }
